c9d9be7db8b1d0ee4e6cb29bdaad3e8d44b5de95
EmilChoparinov <emilchoparinov@gmail.com>
2017-10-25 20:27:52 -0700

    fixed previous 2n card not switching properly

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 //this makes sure that dynamically generated cards have jQuery functionality
 function card_handler() {
-    $('.card').click(function () {
-        console.log("card clicked");
+    $('.card:last-child').click(function () {
         var x = $(this).width();
         $(this).css('width', x);
         $(this).children('p').fadeOut(function () {
             var t = $(this).attr('temp');
             $(this).attr('temp', $(this).text());
             $(this).text(t);
-            console.log($(this).html())
             $(this).parent().children('p').fadeIn();
         });
     });
@@ -18,15 +16,12 @@ $(document).ready(function () {
 
     //since button is not a submit button, this force submits the form
     $('button').click(function () {
-        console.log('button clicked');
         $('form').submit();
     });
 
     //once the form is submitted it injects the html into #right_panel and attaches the card_handler
     $('form').submit(function () {
-        console.log('in submit');
         var data = $(this).serializeArray();
-        console.log(data);
         var myHTML = '<div class="card"><p class="card_name" temp="">'
             + data[0].value + ' ' + data[1].value +
             '</p><p class="card_description" temp="' + data[2].value + '">Click for description!</p></div>';
