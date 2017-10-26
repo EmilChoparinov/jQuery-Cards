8518ab136d2292c29c28c006dff346ebc52c77c9
EmilChoparinov <emilchoparinov@gmail.com>
2017-10-25 20:53:57 -0700

    Added Scrollbar functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function card_handler() {
     $('.card:last-child').click(function () {
         var x = $(this).width();
         $(this).css('width', x);
+        $(this).css('height', $(this).height());
         $(this).children('p').fadeOut(function () {
             var t = $(this).attr('temp');
             $(this).attr('temp', $(this).text());
@@ -22,6 +23,10 @@ $(document).ready(function () {
     //once the form is submitted it injects the html into #right_panel and attaches the card_handler
     $('form').submit(function () {
         var data = $(this).serializeArray();
+        // var hello = 'hello';
+        // cosole.log(`ajjdjdjd ${hello} !`);
+        // var html = `<div class="card"><p class="card_name" temp="">${data[0].value} ${data[1].value}</p><p class="card_description" temp="'${data[2].value}'">Click for description!</p></div>`;
+        // console.log(html);
         var myHTML = '<div class="card"><p class="card_name" temp="">'
             + data[0].value + ' ' + data[1].value +
             '</p><p class="card_description" temp="' + data[2].value + '">Click for description!</p></div>';
